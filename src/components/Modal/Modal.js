0e0df4015b79570126380397ebd6c829c99d47c4
Marya Samsonenko <samashushka7@gmail.com>
2022-01-26 00:21:42 +0200

    add hooks in the modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
 
@@ -6,32 +6,37 @@ import { Overlay, ModalStyled } from "./Modal.styled";
 
 const modalRoot = document.querySelector("#modal-root");
 
-export class Modal extends Component {
-  handleKeydown = (event) => {
-    if (event.code === "Escape") {
-      this.props.onClose();
-    }
-  };
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeydown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeydown);
-  }
-  handleBackdrope = (event) => {
+export const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    window.addEventListener("keydown", (event) => {
+      if (event.code === "Escape") {
+        onClose();
+      }
+    });
+  }, [onClose]);
+
+  useEffect(() => {
+    window.removeEventListener("keydown", (event) => {
+      if (event.code === "Escape") {
+        onClose();
+      }
+    });
+  }, [onClose]);
+
+  const handleBackdrope = (event) => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleBackdrope}>
-        <ModalStyled>{this.props.children}</ModalStyled>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+
+  return createPortal(
+    <Overlay onClick={handleBackdrope}>
+      <ModalStyled>{children}</ModalStyled>
+    </Overlay>,
+    modalRoot
+  );
+};
+
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
   children: PropTypes.node,
